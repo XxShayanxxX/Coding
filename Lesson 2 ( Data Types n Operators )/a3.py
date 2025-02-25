fe6ff7cd25af1fae29247c60184829aa837e0315
XxShayanxxX <balajishayan@gmail.com>
2025-02-25 20:04:58 +0800

    a3

---
+++ Lesson 2 ( Data Types n Operators )/a3.py
@@ -0,0 +1,18 @@
+x = 5
+y = 10 
+
+
+print("add :", x + y)
+print("sub :", x - y)
+print("divide :", x / y)
+print("multiply :", x * y)
+print("floor", x // y)
+print("module:", x % y)
+
+print("x = y ?" , x == y)
+print("x is greater than y:", x > y)
+print("is x less than y ?" , x < y)
+print("x is not to y", x != y)
+
+result = x + y / x * y - 12 + 3 
+print(result)
