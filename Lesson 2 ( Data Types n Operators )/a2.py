d64d8acb3dd45cd881437c6e637e897d7306f606
XxShayanxxX <balajishayan@gmail.com>
2025-02-25 19:54:36 +0800

    a2

---
+++ Lesson 2 ( Data Types n Operators )/a2.py
@@ -0,0 +1,23 @@
+name = " Shayan"
+age  = 14
+is_student = True 
+weight = 34.5
+
+print("My name is", name )
+print("My name is " , type(name))
+print("My age is", age )
+print("My age is " , type(age))
+print(" Hi i am a student ", is_student )
+print("Hi i am a student " , type(is_student))
+print("I am currently weighing", weight)
+print("I am currently weighing" , type(weight))
+
+
+age = str(age)
+print("My age is", age )
+print("My age is " , type(age))
+
+weight = int(weight)
+print("I am currently weighing", weight)
+print("I am currently weighing" , type(weight))
+
