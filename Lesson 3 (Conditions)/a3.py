9f9d1cca2dc4d16c9b9c192888f092a16c47d239
XxShayanxxX <balajishayan@gmail.com>
2025-02-26 19:56:07 +0800

    a3

---
+++ Lesson 3 (Conditions)/a3.py
@@ -0,0 +1,13 @@
+number = int(input("Enter a number:"))
+
+if number < 50 : 
+    print("The number is less than 50")
+    if number%2 ==0 : 
+        print("The number is even")
+    else:
+        print("The number is odd")
+
+else :
+    print("The number is more than 25 ")
+
+
