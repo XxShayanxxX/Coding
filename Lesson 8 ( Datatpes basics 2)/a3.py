b9288e510d862a180d66cbae3609efc7e3d2a170
XxShayanxxX <balajishayan@gmail.com>
2025-03-18 20:11:34 +0800

    eqdq

---
+++ Lesson 8 ( Datatpes basics 2)/a3.py
@@ -0,0 +1,14 @@
+set1 = {"red", "blue"}
+set2 = {"blue", "yellow"}
+
+print("origianl set :")
+print(set1)
+print(set2)
+
+
+setz = set1.intersection(set2)
+print("intersection : " ,setz)
+
+sety = set1.union(set2)
+print("union: ", sety)
+
