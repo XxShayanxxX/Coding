b69c8165d62fe573be1569c53eff2251b384d4a3
XxShayanxxX <balajishayan@gmail.com>
2025-04-08 19:53:35 +0800

    sadwsawd

---
+++ 13 NOTATION/a1.py
@@ -0,0 +1,16 @@
+# Program to show COnstant TIme complexity
+
+def printnumber(n):
+    iterations = 0
+    print("The number input by the user is :", n )
+
+    iterations += 1
+    print("Total iterations are :", iterations,"\n")
+
+
+printnumber(10)
+printnumber(20)
+print("\n With any 'n' the time taken is constant")
+          
+
+          
