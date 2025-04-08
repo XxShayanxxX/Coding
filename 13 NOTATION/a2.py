73a01e4f4521d5e5cb370f5cb85e58d056b8d9cb
XxShayanxxX <balajishayan@gmail.com>
2025-04-08 19:58:00 +0800

    cwada

---
+++ 13 NOTATION/a2.py
@@ -0,0 +1,13 @@
+def Ontime(n):
+    iterations = 0 
+    for i in range(1,n+1):
+        iterations += 1
+
+    print("When n is", n,"Iterations =", iterations)
+
+
+Ontime(20)
+Ontime(30)
+Ontime(35)
+
+print("\n with every n the time take and iterations will increase")
