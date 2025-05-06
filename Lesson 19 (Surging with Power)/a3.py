6f48c50984e332f7a018cf707c7eb6949d058635
XxShayanxxX <balajishayan@gmail.com>
2025-05-06 20:00:10 +0800

    dsad

---
+++ Lesson 19 (Surging with Power)/a3.py
@@ -0,0 +1,20 @@
+def computerPower(x,y):
+
+    result = 1
+
+    while y > 0:
+        if y % 2 == 0:
+            x = x* x
+            y >> = 1
+
+        else:
+            result = result * x 
+            y = y - 1
+
+    return result
+
+x = int(input("Enter a number:"))
+y = int(input("Enter the 2nd number:"))
+
+print("Total is:", computerPower(x,y))
+
