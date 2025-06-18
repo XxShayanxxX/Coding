5062865a4bf19806cd888ea47bf2c85f79e5492c
Shayan <164016339+XxShayanxxX@users.noreply.github.com>
2025-06-18 20:15:07 +0800

    Add files via upload

--- a2.py
+++ a2.py
@@ -1,13 +1,32 @@
-# Program to check if the Nth bit is set or not set 
-def setNor(number,n):
-    # Make a mask varibale by left shifting 1 (k-1) times and check if (n AND mask) equals 1 or 0 
-    if number & (1 << ( n - 1)):
-        print("\nSET")
-    else:
-        print("\nNOTSET")
+#Print the index of subarry with given sum 
 
-
-number = int(input("Enter a number :"))
-n = int(input("Enter a bit"))
-setNor(number,n)
+def subArraySUm(arr,n,sum_):
     
+    for i in range(n):
+        curr_sum = arr[i]
+
+        #Sum of all subarrays starting with i 
+        j = i + 1
+        while j <= n:
+            if curr_sum == sum_ : 
+                print("Sum found between")
+                print("indexes % d and % d"%(i,j-1))
+
+                return 1 
+            
+            if curr_sum > sum_ or j == n:
+                break
+
+            curr_sum + curr_sum + arr[j]
+            j += 1
+
+
+    print("No subarray found")
+    return n
+
+
+arr = [3,6,2,2,56,1,0,9]
+n = len(arr)
+sum_ = 10 
+subArraySUm(arr,n,sum_)
+
