200a488dfbf192f083f2760a1b1ce20263d4ea1b
Shayan <164016339+XxShayanxxX@users.noreply.github.com>
2025-06-24 19:48:40 +0800

    Add files via upload

--- a1.py
+++ a1.py
@@ -1,32 +1,23 @@
-def equibrimPoint(arr):
-    leftsidesum = 0 
-    rightsidesum = 0 
-    n = len(arr)
+#function to seach for an element 
+def search(arr,n ,x):
+    for i in range(0,n):
+        if(arr[i]==x):
+                return i 
+    
+    #If element is not found in this array 
+    #return -1 
+    return -1   
+    #Take arraay input from user 
+    #Enter element to search for 
 
+arr = [2,3,4,10,40]
+x = 10 
+n = len(arr)
 
-    #Go to each indx of the array and check if it an equiliubrium point 
-    for i in range(n):
-        leftsidesum = 0 
-        rightsidesum = 0 
-         
-
-         #get left sum 
-        for j in range(i):
-            leftsidesum += arr[j]
-
-        #get right sum 
-        for j in range(i + 1,n):
-            rightsidesum += arr[j]
-
-
-        # if leftsum and rightsum are same , 
-        #then we are done 
-        if leftsidesum == rightsidesum:
-            return i 
-        
-
-    return -1
-
-arr = [-4,6,2,0,0,1,1]
-print("Element :", arr[equibrimPoint(arr)])
-           
+#Function call 
+result = search(arr,n,x)
+#check returned index and print the output
+if(result == -1):
+    print("Elment is not present in array")
+else: 
+    print("Element is present at index", result)
