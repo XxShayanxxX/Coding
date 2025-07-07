5c85e25a67bf6da2d96eebce1309b0dee4898a82
Shayan <164016339+XxShayanxxX@users.noreply.github.com>
2025-07-07 20:12:30 +0800

    Add files via upload

--- a2.py
+++ a2.py
@@ -1,38 +1,19 @@
-#Program to perform binary search recursively
-def binaryseach(arr,l,r,x):
-    #Check if the length of array is greater than or equal to 0 
-    if r >=1:
+#build in function
+def compare(n1,n2):
+    return str(n1) + str(n2) + str(n1)
 
-        # find the mid element's index 
-        mid = l + (r-1) //2
-
-        #If element is present at the middle itself 
-        if arr[mid] == x:
-            return mid 
-        
-        # IF element is smaller check in left subarray 
-        elif arr[mid] > x:
-            return binaryseach(arr,l,mid-1,x)
-        
-        #Else check in right subarray 
-        else:
-            return binaryseach(arr,mid + 1,r , x )
-        
-    else : 
-
-        #Element is not present in the array 
-        return - 1
+#section sort 
+def largestnumber(nums):
+    for i in range(len(nums),0,-1):
+        tmp = 0 
+        for j in range(i):
+            if not compare(nums[j],nums[tmp]):
+                tmp = j 
+            nums[tmp],nums[i-1] = nums[i-1],nums[tmp]
+        return str(int("".join(map(str,nums))))
     
+
 #Drivers code 
-arr = [2,3,4,10,40]
-x = 10 
-
-#function call 
-result = binaryseach(arr,0,len(arr)-1,x)
-
-if result != 1:
-    print("Element {} is present at index {}".format(x,result))
-
-else:
-    print("Element is not present in arrat")
-
+arr = [3,30,34,5,12]
+print("given array : ",arr)
+print("Largest possible number :",largestnumber(arr))
