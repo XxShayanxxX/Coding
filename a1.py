9584d2633cef153df4b3b6749c632bc2622a4999
Shayan <164016339+XxShayanxxX@users.noreply.github.com>
2025-09-02 19:45:29 +0800

    Add files via upload

--- a1.py
+++ a1.py
@@ -1,17 +1,22 @@
-#python program to change the cases of letters in a string 
-def changeTheCase(s):
-    result = " "
-    for i in s:
-        #chaning lower case to upper case 
-        if i.islower():
-            result = result + i.upper()
-        #upper case to lower case
-        if i.isupper():
-            result = result + i.lower()
+#function to count the frequency of each 
+#character in a string 
 
-    return result 
+def frequency(s):
+    #creating a dictionary to store the frequency of each character 
+        s = s.lower()
+        d = {}
+
+        for i in range(len(s)):
+                #checking if the character is already 
+                #in the dictionary 
+                if s[i] in d.keys():
+                    d[s[i]] += 1 
+                else:
+                    d[s[i]] = 1 
+
+        return d 
 
 #drivers code 
-inp = input("Enter String: ")
-print("String after change lower case to upper case vice-versa")
-print(changeTheCase(inp))
+inp = input("Enter a string: ")
+print(frequency(inp))
+                    
