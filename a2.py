e8ff1d589359baad6af6c26fc5b80778e1afa553
Shayan <164016339+XxShayanxxX@users.noreply.github.com>
2025-09-02 19:57:07 +0800

    Add files via upload

--- a2.py
+++ a2.py
@@ -1,22 +1,35 @@
-def reverse(s):
-    n = len(s)
-
-    li = list(s)
-    for i in range(n//2):
-
-        li[i]   ,li[n-i-1] = li[n-i-1],li[i]
-    return "".join(li)
-#function to check the string is palindrome 
-def checkpalindrome(s):
-    #ignoring case
+# An anagram is a string formed by rearranging the letters of another string.
+# function to count the frequency of each
+# character of a string 
+def frequency(s):
+    #creating a dictonary to store the frequency of each character 
     s = s.lower()
-    rev_string = reverse(s)
-    if s == rev_string:
+    d = {}
+
+    for i in range(len(s)):
+        #checking if the character is already 
+        #in the dictonary 
+
+        if s[i] in d.keys():
+            d[s[i]] += 1 
+            
+        else:
+            d[s[i]] = 1 
+
+    return d 
+
+#function to check Anagram strings 
+def checkAnagram(s1,s2):
+    d_s1 = frequency(s1)
+    d_s2 = frequency(s2)
+
+    if d_s1 == d_s2:
         return True 
     else:
         return False
     
+#drivers code 
+inp1 = input("Enter a string 1: ")
+inp2 = input("Enter a string 2: ")
 
-#drivers code
-inp = input("Enter string: ")
-print(checkpalindrome(inp))
+print(checkAnagram(inp1,inp2))
