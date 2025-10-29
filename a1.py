2989c0cdca27158857fefcc3265ffd68e04b5748
Shayan <164016339+XxShayanxxX@users.noreply.github.com>
2025-10-29 19:56:23 +0800

    Add files via upload

--- a1.py
+++ a1.py
@@ -1,18 +1,35 @@
-#Python program to demonstrate 
-#insert operation in binary search tree 
+#python program to demonstrate 
+#insert and search operation in binary search tree 
 
-#a utility class that represents 
+#A utility class that represents 
 #an individual node in a BST 
+
+
 class Node:
     def __init__(self,key):
-        self.left = None 
         self.right = None 
+        self.left = None 
         self.val = key 
 
-    
+#A utility function to insert 
+#A new node with the given key 
 
-#a utility function to insert 
+def insert(root,key):
+    if root is Node:
+        return Node(key)
+    else:
+        if root.val == key:
+            return root
+#A utility function to insert
 #a new node with the given key 
+
+def insert(root,key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.val == key:
+            return root 
+        
 def insert(root,key):
     if root is None:
         return Node(key)
@@ -20,49 +37,40 @@ def insert(root,key):
         if root.val == key:
             return root 
         elif root.val < key:
-            if root.val == key:
-                return root 
-            
-            elif root.val < key :
-                root.right = insert(root.right,key)
-            else:
-                root.left == insert(root.left,key)
-        return root
+            root.right = insert(root.right,key)
+        else:
+            root.left = insert(root.left,key)
+        return root 
     
-
 #A utility function to do inorder tree traversal 
+
 def inorder(root):
     if root:
         inorder(root.left)
-        print(root.val, end = " ")
+        print(root.val)
+        inorder(root.right)
+    if root:
+        inorder(root.left)
+        print(root.val)
         inorder(root.right)
 
-#Driver program to test the above functions 
-if __name__ == '__main__':
+# Driver program to test the above functions 
+#Let us create the following BSt 
+#50
+#/\
+#30 70
+#/\ /\
+#20 40  60 80 
 
-    #Let us create the following BST
-    #50 
-    #30 70 
-    #20 40 60 80 
+r = Node(50)
+r = insert(r,30)
+r = insert(r,20)
+r = insert(r,40)
+r = insert(r,70)
+r = insert(r,60)
+r = insert(r,80)
 
-    #Let us create the following BST 
-    #50 
-    # 30 70 
-    # 
-    #20 40 60 80 
+#pRINT INORDER  TRAVERSAL OF THE BST 
+inorder(r)
 
-
-    r = Node(50)
-    r = insert(r,30)
-    r = insert(r,20)
-    r = insert(r,40)
-    r = insert(r,70)
-    r = insert(r,60)
-    r = insert(r,80)
-
-    #Print inorder traversal of the BST 
-    inorder(r)
     
-
-
-        
