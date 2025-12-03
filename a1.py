e3aff8f6916e2a7c8bf61dc6a68a016473daf850
Shayan <164016339+XxShayanxxX@users.noreply.github.com>
2025-12-03 19:53:55 +0800

    Add files via upload

--- a1.py
+++ a1.py
@@ -1,119 +1,47 @@
-class Node:
-    def __init__(self,key,value):
-        self.key = key
-        self.value = value
-        self.next = None
+class Graph:
+    def __init__(self):
+        self.nodes = set()
+        self.edges = {}
 
-class HashTable:
-    def __init__(self,capacity):
-        self.capacity = capacity
-        self.size = 0 
-        self.table = [None] * capacity
+    def add_node(self,node):
+        self.nodes.add(node)
 
-    def _hash(self,key):
-        return hash(key) % self.capacity
+
+    def add_edges(self,node1,node2,weight=1):
+        if node1 not in self.nodes:
+            self.nodes.add(node1)
+        if node2 not in self.nodes:
+            self.nodes.add(node2)
+
+        if node1 not in self.edges:
+            self.edges[node1] = set()
+        self.edges[node1].add((node2,weight))
+
+        if node2 not in self.edges:
+            self.edges[node2] = set()
+        self.edges[node2].add((node1,weight))
+
+    def get_nodes(self):
+        return self.nodes
     
-    def insert(self,key,value):
-        index = self._hash(key)
-
-        if self.table[index] is None:
-            self.table[index] = Node(key,value) 
-            self.size += 1 
-
-        else:
-            current = self.table[index]
-            while current:
-                if current.key == key:
-                    current.value = value 
-                    return 
-                
-                current = current.next
-            new_node = Node(key,value)
-            new_node.next = self.table[index]
-            self.table[index] = new_node
-            self.size += 1 
-            
-            
-    def search(self,key):
-        index = self._hash(key)
-
-        current = self.table[index]
-        while current:
-            if current.key == key:
-                return current.value 
-            current = current.next 
-
-        raise KeyError(key)
+    def get_edges(self):
+        return self.edges
+    
+    def __repr__(self):
+        return str(self.nodes) + "->" + str(self.edges)
     
 
-    def remove(self,key):
-        index = self._hash(key)
+#Create a graph 
+graph = Graph()
 
-        previous = None 
-        current = self.table[index]
+#Add nodes
+graph.add_node("A")
+graph.add_node("B")
+graph.add_node("C")
 
-        while current:
-            if current.key == key:
-                if previous:
-                    previous.next = current.next
-                else:
-                    self.table[index] = current.next 
-                self.size -= 1 
-                return 
-            previous = current 
-            current = current.next 
+#Add edges 
+graph.add_edges("A","B")
+graph.add_edges("A","C")
+graph.add_edges("B","C")
 
-        raise KeyError(key)
-    
-    def __len__(self):
-        return self.size
-    
-    def __contains__(self,key):
-        try:
-            self.search(key)
-            return True 
-        except KeyError:
-            return False
-        
-#Driver code 
-if __name__ == "__main__":
-
-    #Create a hash table with a capacitu of 5 
-    ht = HashTable(5)
-
-    #Add some key value pairs
-    #to the hash table 
-    ht.insert("apple",3)
-    ht.insert("banana",2)
-    ht.insert("cherry",5)
-
-    #Check if the has table contains a key 
-    print("apple" in ht) #True 
-    print("durian" in ht) #FaLse
-
-    #Get the value for a key 
-    print(ht.search("banana")) #2 
-
-    #Update the value for a key 
-    ht.insert("banana",4)
-    print(ht.search("banana")) #4
-
-    ht.remove("apple")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(graph)
