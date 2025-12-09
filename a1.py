093f72d73fb341a83748faefdfc6d89d77d05023
Shayan <164016339+XxShayanxxX@users.noreply.github.com>
2025-12-09 20:06:25 +0800

    Add files via upload

--- a1.py
+++ a1.py
@@ -1,38 +1,65 @@
-# A utility function to add an edge in an undirected graph.
-# edge in an undirected graph.
+# Python program for Dijkstra's single 
+# source shortest path algorithm. The program is 
+# for adjacency matrix representation of the graph 
 
-def addEdge(adj,u,v):
-    adj[u].append(v)
-    adj[v].append(u)
+# Library for INT_MAX
+import sys 
 
-# A utility function to do DFS of graph 
-# recursively from a given vertex u 
+class Graph():
 
-def DFSUtil(u,adj,visited):
-    visited[u] = True
-    for i in range(len(adj[u])):
-        if(visited[adj[u][i]] == False):
-          DFSUtil(adj[u][i],adj,visited)
+    def __init__(self,vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)]
+                        for row in range(vertices)]
+        
+    def printSolution(self,dist):
+        print("Vertex \t Distance from Source")
+        for node in range(self.V):
+            print(node,"\t",dist[node])
 
+    def minDistance(self,dist,sptSet):
 
-#Returns count of tree is the 
-#forest given as adjacency list.
+        min = sys.maxsize
 
-def countTrees(adj,V):
-    visited = [False]*V
-    res = 0 
-    for u in range(V):
-        if(visited[u] == False):
-            DFSUtil(u,adj,visited)
-            res += 1
-    return res
+        for u in range(self.V):
+            if dist[u] < min and sptSet[u] == False:
+                min = dist[u]
+                min_index = u 
+
+        return min_index
+    
+    def dijkstra(self,src):
+
+        dist = [sys.maxsize] * self.V
+        dist[src] = 0 
+        sptSet = [False] * self.V
+
+        for count in range(self.V):
+
+            x = self.minDistance(dist,sptSet)
+
+            #Put the minimum distance vertex in the 
+            #shorest path tree 
+            sptSet[x] = True 
+
+            for y in range(self.V):
+                if self.graph[x][y] > 0 and sptSet[y] == False and \
+                        dist[y] > dist[x] + self.graph[x][y]:
+                    dist[y] = dist[x] + self.graph[x][y]
+
+        self.printSolution(dist)
+
 #Drivers code 
 if __name__ == "__main__":
-
-    V = 5 
-    adj = [[]for i in range(V)]
-    addEdge(adj,0,1)
-    addEdge(adj,0,2)
-    addEdge(adj,3,4)
-    print(countTrees(adj,V))
-
+    g = Graph(9)
+    g.graph = [[0,4,0,0,0,0,0,8,0],
+               [4,0,8,0,0,0,0,11,0],
+               [0,8,0,7,0,4,0,0,2],
+               [0,0,7,0,9,14,0,0,0],
+               [0,0,0,9,0,10,0,0,0],
+               [0,0,4,14,10,0,2,0,0],
+               [0,0,0,0,0,2,0,1,6],
+               [8,11,0,0,0,0,1,0,7],
+               [0,0,2,0,0,0,6,7,0]]
+    
+    g.dijkstra(0)
