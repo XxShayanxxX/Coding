90876d07e0ddbb7395093c1ff0417533cde0051e
Shayan <164016339+XxShayanxxX@users.noreply.github.com>
2025-12-09 12:41:35 +0800

    Add files via upload

--- a2.py
+++ a2.py
@@ -1,39 +1,47 @@
-def dfs(graph,start):
-    """
-    Performs a depth-first search on the given graph, starting at given node 
+#Function to find the minimum 
+#number of rabbits in the forest 
 
-    Args:
-        graph: The graph to search 
-        start: The node to start the search at 
+def minNumberOfRabbits(answer,N):
 
-    Returns:
-        A list of the nodes visited by the search,in the order in which they were visited
+    #Initialize map
+    map = {}
 
-    """
+    #Traverse array and map arr[i]
+    #to the number of occurrences 
 
-    visited = set()
-    stack = [start]
+    for a in range(N):
 
-    while stack:
-        node = stack.pop()
+        if answer[a] in map:
+            map[answer[a]] += 1 
+        else:
+            map[answer[a]] = 1 
 
-        if node not in visited:
-            visited.add(node)
-            for neighbour in graph[node]:
-                stack.append(neighbour)
 
-    return visited
+    count = 0 
 
-graph = {
-    'A' : ['B','C'],
-    'B' : ['D'],
-    'C' : ['E'],
-    'D' : [],
-    'E' : []
+#Find the number groups and 
+#no. of rabbits in each group 
+    for a in map:
 
-}        
+        x = a 
+        y = map[a]
 
-visited = dfs(graph,'A')
+            #Find number of groups and 
+            #multiply them with number 
+            #of rabbits in each group 
+        if(y %(x + 1) == 0):
+            count = count + (y // (x + 1)) * (x + 1)
+        else:
+            count = count + ((y // (x + 1 ))+ 1) * (x + 1)
 
-print(visited)
-        
+#count gives minimum number 
+#of rabbits in the forest 
+    return count
+
+#Driver code 
+arr = [2,2,0]
+N = len(arr)
+
+#Function Call 
+print(minNumberOfRabbits(arr,N))
+
