272f383e255994ab692e5039f7e6e073ded21e10
Shayan <164016339+XxShayanxxX@users.noreply.github.com>
2025-12-15 17:53:37 +0800

    Add files via upload

--- a1.py
+++ a1.py
@@ -1,67 +1,87 @@
-#Number of vertices in the graph 
-V = 4 
+import time 
 
-#Define infinity as the large 
-#enough value. The value will be 
-#used for vertices not connected to each other 
-INI = 99999
+#print the board
+def print_board(board,n):
+    for i in range(n):
+        for j in range(n):
+            print(board[i][j], end = " ")
+        print()
 
-#Solves all pair shortest pair 
-#via Floyd Warshall Algorithm
+# Joining '.' and 'Q'
+# making combined 2D Array 
+# For output in desired format 
+def add_sol(board,ans,n):
+    temp = []
 
-def floydWarshall(graph):
-    """ dist[][] will be the output
-    matrix that will finally
-        have the shortest distances
-        between every pair of vertices"""
-    """ initiallizing the solution matrix 
-    same as input graph matrix 
-    OR we can say that the initial 
-    value of shortest distances 
-    are based on shortest paths considering no
-    intermideate vertices """
-
-    dist = list(map(lambda i: list(map(lambda j: j,i)),graph))
-
-    for i in range(V):
-
-        #Pick all vertices as destination for the 
-        #above picked source 
-        for j in range(V):
-        
-                for k in range(V):
-                     
-
-                    #If vertex K is on the shortest path from 
-                    # i to j, then update the value of dist[i][j]
-                    dist[i][j] = min(dist[i][j],
-                        dist[i][k] + dist[k][j]
-                        )
-    printSolution(dist)
-
-# A utility function to print the solution 
-def printSolution(dist):
-    print("Following matrix shows the shortest diostances\ between every pair of vertices")
-    for i in range(V):
-         for j in range(V):
-            if(dist[i][j] == INI):
-               print("%7s" % ("INI"),end=" ")
-            else:
-                print("%7d\t" % (dist[i][j]),end=' ')
-            if j == V-1:
-                print()
-
-                print("%7d\t" % (dist[i][j]), end=" ")
-            if j == V-1:
-                print()
+    for i in range(n):
+        string = ""
+        for j in range(n):
+            string += board[i][j]
+        temp.append(string)
+    ans.append(temp)
 
 
-#Drivers code 
-if __name__ =="__main__":
 
-    graph = [[0,5,INI,10],
-        [INI,0,3,INI],
-        [INI,INI,0,1],
-        [INI,INI,INI,0]]
-             
-floydWarshall(graph)
+def is_safe(row, col, board,n):
+    x = row 
+    y = col 
+
+    while(x >= 0):
+        if board[x][y] == "Q":
+            return False 
+        else:
+            x -= 1 
+
+
+    x = row 
+    y = col
+    while(y < n and x >= 0):
+        if board[x][y] == "Q":
+            return False
+        else:
+            y += 1 
+            x -= 1 
+
+    x = row 
+    y = col 
+
+    while(y >= 0 and x >= 0):
+        if board[x][y] == "Q":
+            return False
+        else:
+            y -= 1 
+            x -= 1 
+
+    return True
+    
+
+def solveNQueens(row,ans,board,n):
+
+    if row == n:
+        add_sol(board,ans,n)
+        return 
+    
+
+    for col in range(n):
+
+        if is_safe(row,col,board,n):
+
+            board[row][col] = "Q"
+            solveNQueens(row+1,ans,board,n)
+
+
+            board[row][col] = "."
+if __name__ == "__main__":
+    n = 4 
+    board = [["." for i in range(n)] for j in range(n)]
+
+    ans = []
+    solveNQueens(0,ans,board,n)
+
+    if ans == []:
+        print("Solution does not exsist")
+    else:
+        print(len(ans))
+        print(f"Out of {len(ans)} solution is following ")
+        print_board(ans[0],n)
+
