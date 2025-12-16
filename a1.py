aa1e4114dabe97aa01b5de1a38604e57f956e9f6
Shayan <164016339+XxShayanxxX@users.noreply.github.com>
2025-12-16 20:09:53 +0800

    Add files via upload

--- a1.py
+++ a1.py
@@ -1,87 +1,70 @@
-import time 
+# N is the size of the 2D matrix N*N
+N = 9 
 
-#print the board
-def print_board(board,n):
-    for i in range(n):
-        for j in range(n):
-            print(board[i][j], end = " ")
+#A utility function to print grid 
+def printing(arr):
+    for i in range(N):
+        for j in range(N):
+            print(arr[i][j], end = " ")
+
         print()
 
-# Joining '.' and 'Q'
-# making combined 2D Array 
-# For output in desired format 
-def add_sol(board,ans,n):
-    temp = []
 
-    for i in range(n):
-        string = ""
-        for j in range(n):
-            string += board[i][j]
-        temp.append(string)
-    ans.append(temp)
+def isSafe(grid, row, col, num):
 
 
-
-def is_safe(row, col, board,n):
-    x = row 
-    y = col 
-
-    while(x >= 0):
-        if board[x][y] == "Q":
-            return False 
-        else:
-            x -= 1 
-
-
-    x = row 
-    y = col
-    while(y < n and x >= 0):
-        if board[x][y] == "Q":
+    for x in range(9):
+        if grid[row][x] == num:
             return False
-        else:
-            y += 1 
-            x -= 1 
+        
 
-    x = row 
-    y = col 
+    startRow = row - row % 3
+    stratCol = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if grid[i + startRow][j + stratCol] == num:
+                return False
+    return True 
 
-    while(y >= 0 and x >= 0):
-        if board[x][y] == "Q":
-            return False
-        else:
-            y -= 1 
-            x -= 1 
+def solveSudoku(grid, row, col):
 
-    return True
+
+    if (row == N - 1 and col == N):
+        return True 
     
+    if col == N:
+        row += 1 
+        col = 0 
 
-def solveNQueens(row,ans,board,n):
-
-    if row == n:
-        add_sol(board,ans,n)
-        return 
     
+    if grid[row][col] > 0:
+        return solveSudoku(grid, row, col + 1)
+    for num in range(1, N + 1, 1):
 
-    for col in range(n):
+        if isSafe(grid,row,col,num):
+            grid[row][col] = num
 
-        if is_safe(row,col,board,n):
+            if solveSudoku(grid,row,col + 1):
+                return True 
+        
 
-            board[row][col] = "Q"
-            solveNQueens(row+1,ans,board,n)
+        grid[row][col] = 0 
+    return False
+
+grid = [[3,0,6,5,0,8,4,0,0],
+        [5,2,0,0,0,0,0,0,0],
+        [0,8,7,0,0,0,0,3,1],
+        [0,0,3,0,1,0,0,8,0],
+        [9,0,0,8,6,3,0,0,5],
+        [0,5,0,0,9,0,6,0,0],
+        [1,3,0,0,0,0,2,5,0],
+        [0,0,0,0,0,0,0,7,4],
+        [0,0,5,2,0,6,3,0,0]]
+
+if(solveSudoku(grid, 0,0)):
+    printing(grid)
+else:
+    print("no solution exists")
 
 
-            board[row][col] = "."
-if __name__ == "__main__":
-    n = 4 
-    board = [["." for i in range(n)] for j in range(n)]
-
-    ans = []
-    solveNQueens(0,ans,board,n)
-
-    if ans == []:
-        print("Solution does not exsist")
-    else:
-        print(len(ans))
-        print(f"Out of {len(ans)} solution is following ")
-        print_board(ans[0],n)
 
