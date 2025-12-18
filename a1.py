884c35e2d94fd0965c3eaed4471423833db7d9cd
Shayan <164016339+XxShayanxxX@users.noreply.github.com>
2025-12-18 12:43:20 +0800

    Add files via upload

--- a1.py
+++ a1.py
@@ -1,70 +1,68 @@
-# N is the size of the 2D matrix N*N
-N = 9 
+#python program to solve rat in a maze 
+#problem using back tracking
 
-#A utility function to print grid 
-def printing(arr):
-    for i in range(N):
-        for j in range(N):
-            print(arr[i][j], end = " ")
+#Maze size 
+n = 4 
 
-        print()
+# A utlity function to check if x,y is valid 
+# index for N*N Maze
 
-
-def isSafe(grid, row, col, num):
-
-
-    for x in range(9):
-        if grid[row][x] == num:
-            return False
-        
-
-    startRow = row - row % 3
-    stratCol = col - col % 3
-    for i in range(3):
-        for j in range(3):
-            if grid[i + startRow][j + stratCol] == num:
-                return False
-    return True 
-
-def solveSudoku(grid, row, col):
-
-
-    if (row == N - 1 and col == N):
+def isValid(n,maze,x,y,res):
+    if x >= 0 and x < n and y < n and maze[x][y] == 1 and res[x][y] == 0:
         return True 
-    
-    if col == N:
-        row += 1 
-        col = 0 
+    return False 
 
-    
-    if grid[row][col] > 0:
-        return solveSudoku(grid, row, col + 1)
-    for num in range(1, N + 1, 1):
+# A recursive utility function to solve Maze problem
 
-        if isSafe(grid,row,col,num):
-            grid[row][col] = num
+def RatMaze(n,maze,move_x,move_y,x,y,res):
+    #if (x,y is goal) return True 
+    if x == n-1 and y == n-1 :
+        return True 
+    for i in range(4):
+        #Generate new value of x 
+        x_new = x + move_x[i]
 
-            if solveSudoku(grid,row,col + 1):
+        #Generate new value of y
+        y_new = y + move_y[i]
+
+        #Check if maze[x][y] is valid 
+    if isValid(n,maze,x_new,y_new,res):
+
+        #mark x,y as part of solution path 
+        res[x_new][y_new] = 1 
+        if RatMaze(n,maze,move_x,move_y,x_new,y_new,res):
                 return True 
+        res[x_new][y_new] = 0 
+    return False 
+
+def solveMaze(maze):
+     #Creating a 4*4 2-D list 
+    res = [[0 for i in range(n)] for i in range(n)]
+    res[0][0] = 1 
+
+    #x matric for each direction 
+    move_x = [-1,1,0,0]
+
+    #y matrix for each direction
+    move_y = [0,0,-1,1]
+
+    if RatMaze(n,maze,move_x,move_y,0,0,res):
+        for i in range(n):
+            for j in range(n):
+                print(res[i][j], end = " ")
+            print()
+    else:
+        print("Solution does not exsist")
+
+#Drivers program top test above function 
+if __name__ == "__main__":
+
+        #Initialising the maze 
+    maze = [[1,0,0,0],
+            [1,1,0,1],
+            [0,1,0,0],
+            [1,1,1,1]]
+    solveMaze(maze)
+
         
-
-        grid[row][col] = 0 
-    return False
-
-grid = [[3,0,6,5,0,8,4,0,0],
-        [5,2,0,0,0,0,0,0,0],
-        [0,8,7,0,0,0,0,3,1],
-        [0,0,3,0,1,0,0,8,0],
-        [9,0,0,8,6,3,0,0,5],
-        [0,5,0,0,9,0,6,0,0],
-        [1,3,0,0,0,0,2,5,0],
-        [0,0,0,0,0,0,0,7,4],
-        [0,0,5,2,0,6,3,0,0]]
-
-if(solveSudoku(grid, 0,0)):
-    printing(grid)
-else:
-    print("no solution exists")
-
-
 
