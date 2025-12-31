c4b1200e0af6145aabf4759996f2c8d42e33f207
Shayan <164016339+XxShayanxxX@users.noreply.github.com>
2025-12-31 11:46:46 +0800

    Add files via upload

--- homework.py
+++ homework.py
@@ -1,6 +1,17 @@
-def subset_sum(a,s):
-    if s == 0: return True
-    if not a: return False
-    return subset_sum(a[1:],s) or subset_sum(a[1:],s-a[0])
+def minimize_job(jobs):
 
-print(subset_sum([3,34,4,12,4,2],9))
+    order = sorted(range(len(jobs)), key=lambda i: jobs[i][1])
+    
+    t = 0 
+    total = 0
+    for i in order:
+        t += jobs[1][0]
+        total += max(0,t-jobs[i][1])
+    
+    return order,total
+
+jobs = [(4,7),(2,5),(3,9),(6,8)]
+order, tardiness = minimize_job(jobs)
+
+print("Time work is given :" ,order)
+print("total tardiness : ", tardiness) 
